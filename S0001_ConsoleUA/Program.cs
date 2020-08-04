edc057ae5d596bf91809fa431be48f5b8badcb1b
HanNabu <uniondirfolder@gmail.com>
2020-08-04 16:05:13 +0300

    The Singleton Design Pattern - Part of the Gang of Four
    -> ++coin :)

---
+++ S0001_ConsoleUA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace S0001_CUI_SingletonDemo
+{
+    class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine("The next server is: " + servers.GetNextServer());
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+
+            Console.ReadLine();
+        }
+        private static void Host1GetNextServer() 
+        {
+            Console.WriteLine("The next server is: " + host1List.GetNextServer());
+        }
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host2List.GetNextServer());
+        }
+    }
+}
---
+++ S0001_ConsoleUA/TableServers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//The Singleton Design Pattern - Part of the Gang of Four
+//https://www.youtube.com/watch?v=ggqjVuJ0g_8
+
+//look->[Blazor - ConfigureServices(IServiceCollection services)]<- how
+namespace S0001_CUI_SingletonDemo
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+        private TableServers()
+        {
+            servers.Add("Tim");
+            servers.Add("Sue");
+            servers.Add("Mary");
+            servers.Add("Bob");
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+        public string GetNextServer() 
+        {
+            string output = servers[nextServer];
+
+            nextServer += 1;
+
+            if (nextServer >= servers.Count) 
+            {
+                nextServer = 0;
+            }
+
+            return output;
+        }
+    }
+}
