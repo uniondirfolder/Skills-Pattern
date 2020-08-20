e75991f297b145c061785882ca06d414d591d863
HanNabu <uniondirfolder@gmail.com>
2020-08-20 15:49:35 +0300

    State representetions resources

--- Stateless/StateRepresentation.cs
+++ Stateless/StateRepresentation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Stateless
@@ -12,7 +13,43 @@ namespace Stateless
             readonly IDictionary<TTrigger, ICollection<ATriggerBehaviour>> _triggerBehaviours =
                 new Dictionary<TTrigger, ICollection<ATriggerBehaviour>>();
 
-            readonly ICollection<Action<Transiti>>
+            readonly ICollection<Action<Transition, object[]>> _entryActions = new List<Action<Transition, object[]>>();
+            readonly ICollection<Action<Transition>> _exitActions = new List<Action<Transition>>();
+
+            StateRepresentation _superstate; //null
+
+            readonly ICollection<StateRepresentation> _substates = new List<StateRepresentation>();
+
+            public StateRepresentation(TState state) 
+            {
+                _state = state;
+            }
+
+            bool TryFindLocalHandler(TTrigger trigger, out ATriggerBehaviour handler) 
+            {
+                ICollection<ATriggerBehaviour> possible;
+                if (!_triggerBehaviours.TryGetValue(trigger, out possible))
+                {
+                    handler = null;
+                    return false;
+                }
+
+                var actual = possible.Where(at => at.IsGuardConditionMet).ToArray();
+
+                if (actual.Count() > 1)
+                    throw new InvalidOperationException(
+                        string.Format(State))
+            }
+            public bool TryFindHandler(TTrigger trigger, out ATriggerBehaviour handler) 
+            {
+                return(Tr)
+            }
+
+            public bool CanHandle(TTrigger trigger) 
+            {
+                ATriggerBehaviour unused;
+                return TryFi
+            }
         }
     }
     
